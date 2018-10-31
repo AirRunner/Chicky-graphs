cb2317953c52772f5418a1b55850535443a6d544
Vic8t <vic8.taillieu@gmail.com>
2018-10-31 19:54:57 +0100

    basic functions on graphs

--- tests/graphs.c
+++ tests/graphs.c
@@ -8,7 +8,7 @@ typedef struct edge {
 } Edge;
 
 typedef struct node {
-    char infected;
+    int infected;
     struct edge* list;
 } Node;
 
@@ -31,10 +31,9 @@ Graph* createGraph(int size) {
 
     graph->array = malloc(size * sizeof(Node)); 
     for(int i = 0; i < size; i++) {
-        graph->array[i].infected = 0;
+        graph->array[i].infected = -1;
         graph->array[i].list = NULL;
     }
-
     return graph;
 } 
 
@@ -52,16 +51,65 @@ void addEdge(Graph* graph, int src, int dest, int weight) {
     newEdge = createEdge(src, weight); 
     newEdge->next = graph->array[dest].list; 
     graph->array[dest].list = newEdge;
-} 
+}
+
+void deleteEdge(Graph* graph, int src, int dest) {
+    Edge* edge = graph->array[src].list;
+    Edge* prev = NULL;
+    if(edge != NULL){
+        if(edge->dest == dest){
+            graph->array[src].list = graph->array[src].list->next;
+            free(edge);
+        }
+        else{
+            while(edge->dest != dest && edge->next != NULL){
+                prev = edge;
+                edge = edge->next;
+            }
+            if(edge->dest == dest){
+                prev->next = edge->next;
+                free(edge);
+            }
+        }
+    }
+}
+
+int createNode(Graph* graph, int infected) {
+    int i = 0;
+    while(graph->array[i].infected != -1 && i < graph->size)
+        i++;
+    if(i == graph->size)
+        printf("Maximum number of nodes reached");
+    else
+        graph->array[i].infected = infected;
+    return i;
+}
+
+void deleteNode(Graph* graph, int id) {
+    graph->array[id].infected = -1;
+    Edge* tmp;
+    while(graph->array[id].list != NULL) {
+       tmp = graph->array[id].list;
+       graph->array[id].list = graph->array[id].list->next;
+       tmp->next = NULL;
+       free(tmp);
+    }
+
+    for(int i = 0; i < graph->size; i++){
+        deleteEdge(graph, i, id);
+    }
+}
 
 // Print the adjacency list, representation of graph
 void printGraph(Graph* graph) {
     for(int i = 0; i < graph->size; i++) {
-        Edge* pCrawl = graph->array[i].list;
-        printf("\n Adjacency list of vertex %d\n ", i);
-        while(pCrawl) {
-            printf("-> %d", pCrawl->dest);
-            pCrawl = pCrawl->next;
+        if(graph->array[i].infected != -1){
+            Edge* pCrawl = graph->array[i].list;
+            printf("\n Adjacency list of vertex %d\n ", i);
+            while(pCrawl) {
+                printf("-> %d", pCrawl->dest);
+                pCrawl = pCrawl->next;
+            }
         }
         printf("\n");
     }
@@ -69,17 +117,57 @@ void printGraph(Graph* graph) {
 
 void printNodes(Graph* graph) {
     for(int i = 0; i < graph->size; i++) {
-        printf("\nNode %d: ", i);
-        if(graph->array[i].infected)
-            printf("infected");
-        else
-            printf("not infected");
+        if(graph->array[i].infected != -1){
+            printf("\nNode %d: ", i);
+            if(graph->array[i].infected)
+                printf("infected");
+            else
+                printf("not infected");
+        }
     }
     printf("\n");
 }
 
+void contamination(Graph* graph) {
+    int i = 0;
+    for(i = 0; i < graph->size; i++){
+        if(graph->array[i].infected == 1){
+            Edge* edge = graph->array[i].list;
+            while(edge){
+                if(graph->array[edge->dest].infected != 1)
+                    graph->array[edge->dest].infected = 2;
+                edge = edge->next;
+            }
+        }
+    }
+
+    for(i = 0; i < graph->size; i++){
+        if(graph->array[i].infected == 1)
+            deleteNode(graph, i);
+        else if(graph->array[i].infected == 2)
+            graph->array[i].infected = 1;
+    }
+}
+
+int isEmpty(Graph* graph) {
+    int empty = 1;
+    for(int i = 0; i < graph->size; i++){
+        if(graph->array[i].infected != -1)
+            empty = 0;
+    }
+    return empty;
+}
+
 int main() {
     Graph* graph = createGraph(5);
+
+    createNode(graph, 1);
+    createNode(graph, 0);
+    createNode(graph, 0);
+    createNode(graph, 0);
+    createNode(graph, 0);
+    createNode(graph, 0);
+
     addEdge(graph, 0, 1, 1);
     addEdge(graph, 0, 4, 1);
     addEdge(graph, 1, 2, 1);
@@ -88,9 +176,16 @@ int main() {
     addEdge(graph, 2, 3, 1);
     addEdge(graph, 3, 4, 1);
 
-    // print the adjacency list representation of the graph
-    printGraph(graph);
-    printNodes(graph);
+    // Contamination
+    while(!isEmpty(graph)){
+        printGraph(graph);
+        printNodes(graph);
+    
+        contamination(graph);
+    
+        printNodes(graph);
+    }
+    
     system("pause");
     return 0;
 }
